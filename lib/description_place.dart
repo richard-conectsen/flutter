4b6e389c09790bffc17bfbc5d6a4cc7d59ee725d
richard <richard.conectsen@gmail.com>
2022-04-01 09:29:26 -0500

    button with gradient adding to the description_place

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget {
 
@@ -94,9 +95,11 @@ class DescriptionPlace extends StatelessWidget {
         );
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         title_stars,
         parrafo,
+        ButtonPurple("Navigate")
       ],
     );
     //throw UnimplementedError();
